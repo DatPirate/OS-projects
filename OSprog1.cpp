e9d8a7dc8aff1c93d711323b3c072eb0d9721f3e
DatPirate <116280939+DatPirate@users.noreply.github.com>
2024-03-29 10:50:52 +0300

    Код для мультипоточного перемножения матриц 3x3
    0 - без мультипоточности
    1 - с мультипоточностью

---
+++ OSprog1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <thread>
+#include <ctime>
+using namespace std;
+int mx1[3][3] = { {1, 2, 3},{4, 5, 6},{7, 8, 9} };
+int mx2[3][3] = { {2, 6, 8},{3, 7, 9},{1, 4, 5} };
+int mxres[3][3];
+
+void pr3() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+                mxres[i][j] += mx1[i][0] * mx2[0][j] + mx1[i][1] * mx2[1][j] + mx1[i][2] * mx2[2][j];
+        }
+    }
+}
+
+void prmx() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << mxres[i][j] << " ";
+        }
+    }
+}
+
+int main()
+{
+    int start_time = clock();
+    std::thread th1(pr3);
+    th1.join();
+    std::thread th2(prmx);
+    th2.join();
+    int end_time = clock();
+    cout << "\n" << end_time - start_time;
+}
