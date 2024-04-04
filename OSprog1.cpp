3d3e9fbc84096b242017ebd34b06a6b747593325
DatPirate <116280939+DatPirate@users.noreply.github.com>
2024-04-04 20:58:19 +0300

    prog1 updated

--- OSprog1.cpp
+++ OSprog1.cpp
@@ -4,31 +4,53 @@
 using namespace std;
 int mx1[3][3] = { {1, 2, 3},{4, 5, 6},{7, 8, 9} };
 int mx2[3][3] = { {2, 6, 8},{3, 7, 9},{1, 4, 5} };
-int mxres[3][3];
+int mxres1[3][3];
+int mxres2[3][3];
 
-void pr3() {
+void mnmx2(int a) {
     for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-                mxres[i][j] += mx1[i][0] * mx2[0][j] + mx1[i][1] * mx2[1][j] + mx1[i][2] * mx2[2][j];
-        }
+        mxres2[a][i] = mx1[a][0] * mx2[0][i] + mx1[a][1] * mx2[1][i] + mx1[a][2] * mx2[2][i];
+    }
+}
+
+void mnmx1(int a) {
+    for (int i = 0; i < 3; i++) {
+        mxres1[a][i] = mx1[a][0] * mx2[0][i] + mx1[a][1] * mx2[1][i] + mx1[a][2] * mx2[2][i];
     }
 }
 
 void prmx() {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            cout << mxres[i][j] << " ";
+            cout << mxres1[i][j] << " ";
         }
+        cout << "\n";
+    }
+    cout << "\n";
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << mxres2[i][j] << " ";
+        }
+        cout << "\n";
     }
 }
 
 int main()
 {
     int start_time = clock();
-    std::thread th1(pr3);
-    th1.join();
-    std::thread th2(prmx);
-    th2.join();
+    mnmx1(0);
+    mnmx1(1);
+    mnmx1(2);
     int end_time = clock();
-    cout << "\n" << end_time - start_time;
+    cout << "Without multithread:\n" << end_time - start_time << "\n";
+    start_time = clock();
+    std::thread th1(mnmx2, 0);
+    std::thread th2(mnmx2, 1);
+    std::thread th3(mnmx2, 2);
+    th1.join();
+    th2.join();
+    th3.join();
+    end_time = clock();
+    cout << "With multithread\n" << end_time - start_time << "\n";
+    prmx();
 }
